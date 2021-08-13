ac0a009a9d697d7b17396662a89cbe24dc0fac27
Simon Kim <mrsimonkim@gmail.com>
2021-08-12 19:06:00 -0700

    omitted the pushing steps.

--- prep-javascript-for-loops/main.js
+++ prep-javascript-for-loops/main.js
@@ -1,19 +1,11 @@
 // create your loops here.
-var counter = 0;
-var numbers = [];
 for (let i = 0; i < 10; i++) {
-  counter =
-    numbers.push(counter);
+  console.log(i);
 }
-console.log(numbers);
 
-var counter = 0;
-var numbers = [];
-for (let i = 0; i < 10; i++) {
-  counter += 2;
-  numbers.push(counter);
+for (let i = 0; i < 20; i += 2) {
+  console.log(i);
 }
-console.log(numbers);
 
 for (let i = 100; i >= 1; i--) {
   console.log('Time till explosion' + ' ' + i);
